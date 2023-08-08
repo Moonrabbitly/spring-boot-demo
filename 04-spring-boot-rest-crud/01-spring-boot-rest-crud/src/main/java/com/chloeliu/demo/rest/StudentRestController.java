a4e3f8fadd4d2950fdcbca99d90768e151eedc39
Moonrabbitly <yiliu.jobapp@gmail.com>
2023-08-07 21:38:59 -0700

    lec 110

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/chloeliu/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/chloeliu/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.chloeliu.demo.rest;
 
 import com.chloeliu.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,8 +34,12 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
         // just index into the list...keep it simple for now
-        return theStudents.get(studentId);
 
+        // check the studentId again the list size
+        if ((studentId >= theStudents.size()) || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+        return theStudents.get(studentId);
     }
 
 }
