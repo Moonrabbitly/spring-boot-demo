a76caad1d627be8c700dee9c0743c25222064537
Moonrabbitly <yliu3083@gatech.org>
2023-08-07 17:25:15 -0700

    lec 104

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/chloeliu/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/chloeliu/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.chloeliu.demo.rest;
 import com.chloeliu.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +30,13 @@ public class StudentRestController {
     public List<Student> getStudents() {
         return theStudents;
     }
+
+    //define an endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        // just index into the list...keep it simple for now
+        return theStudents.get(studentId);
+
+    }
+
 }
