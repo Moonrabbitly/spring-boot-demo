2fac2a94480931430c8a98a8489dfefd7b8002dc
Moonrabbitly <yiliu.jobapp@gmail.com>
2023-09-09 00:08:12 -0700

    thymeleaf

---
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/thymeleafdemo/src/test/java/com/chloeliu/springboot/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chloeliu.springboot.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
